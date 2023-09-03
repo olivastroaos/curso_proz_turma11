ab533a8766466a229d34e6a317d508384bf4010c
Adriano Santos <aos19@hotmail.com>
2023-09-03 20:43:55 -0300

    novas aulas

---
+++ Aulas_Assincronas/Aula 15 - Conexao-html-js/script.js
@@ -0,0 +1,17 @@
+//let logo = document.getElementById("logo");
+//console.log(logo)
+
+let listaRedesFooter = document.querySelectorAll("footer .lista_redes li")
+//console.log(listaRedesFooter)
+
+function percorrerArray(lista){
+    for(let i = 0; i < lista.length; i++){
+        //console.log("Elemento numero: " + i);
+        console.log(lista[i])
+    }
+}
+
+percorrerArray(listaRedesFooter)
+
+
+
---
+++ Material_Assincrono/JS2_Modulo1/script.js
@@ -0,0 +1,4 @@
+let div = document.querySelector("div");
+div.style.color = "Blue"
+div.style.color = ""
+div.style.fontSize = "16px"
